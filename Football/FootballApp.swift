875ac5c32de2ad7261bcdedd23394ac5cc2f7f88
JW <worladge@icloud.com>
2025-01-25 14:50:52 +1100

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by James Worladge on 25/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
