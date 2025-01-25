3dfed03cb2646721da8ea8fcab6068070e4465f5
JW <worladge@icloud.com>
2025-01-25 22:58:06 +1100

    Prof.G challenge to get the Football app on GitHub!

--- Football/ContentView.swift
+++ Football/ContentView.swift
@@ -10,15 +10,32 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What is Footabll to You?")
+                .foregroundStyle(.green)
+                .fontWeight(.ultraLight)
+                .font(.largeTitle)
+            
+            HStack {
+                Image(systemName: "figure.american.football")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.blue)
+                
+                Image(systemName: "figure.australian.football")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.indigo)
+                
+                Image(systemName: "figure.soccer")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.purple)
+            }
+            .padding()
         }
-        .padding()
     }
 }
+    #Preview {
+        ContentView()
+    }
 
-#Preview {
-    ContentView()
-}
